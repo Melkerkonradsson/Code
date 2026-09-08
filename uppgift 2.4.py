ec34dfb54f5b556d784946fe3c9fd85e6edd7f9e
Melker <melker.konradsson@elev.ga.ntig.se>
2026-09-08 13:19:45 +0200

    uppgift 2.4

--- uppgift 2.4.py
+++ uppgift 2.4.py
@@ -1,1 +1,3 @@
-mile=1,609 (int(input ("")))
+gallon_mile=int(input("ange förbrukningen i gallon per mile"))
+liter_mil=gallon_mile *1.609/3.785
+print("bränsle förbrukining liter per mil ",liter_mil,"")
