e41c6363865f0b7d012f08d13f6c62e2f2aba7c0
Melker <melker.konradsson@elev.ga.ntig.se>
2026-09-08 13:10:12 +0200

    Uppgift 2.2

--- Uppfigt 2.3.py
+++
@@ -1,3 +0,0 @@
-farhenheit=int (input("hur många grader?"))
-Celsius=(farhenheit-32)*5/9
-print(Celsius)
--- uppgift 2.2.py
+++
@@ -1,4 +0,0 @@
-radie=int(input('ge mig radien'))
-import math
-volym=4*math.pi*radie**2/3
-print('volymen är' , (4*math.pi*radie**2/3))
