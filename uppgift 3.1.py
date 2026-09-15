be8ac2aaae20d42b75ab37b7a1ddd1205d1f235c
Melker <melker.konradsson@elev.ga.ntig.se>
2026-09-15 13:36:49 +0200

    uppgift 3.1

---
+++ uppgift 3.1.py
@@ -0,0 +1,8 @@
+tid = int(input("hur länge pratar du i telefon i månaden")) 
+
+if tid < 33: 
+    print("kontant är bäst för dig")
+elif tid >  66: 
+    print("du borde skaffa Plus")
+else:
+   print("Du borde skaffa Normal")
