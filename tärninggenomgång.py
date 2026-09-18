c60bb010a3fb65837493f2eb5df22105dae483c7
Melker <melker.konradsson@elev.ga.ntig.se>
2026-09-18 10:30:32 +0200

    tärning.py

---
+++ tärninggenomgång.py
@@ -0,0 +1,9 @@
+import random 
+ditt_slag=random.randint(1,6)
+datorns_slag=random.randint(1,6)
+print(f"du slog: {ditt_slag}")
+print(f"datorn slog: {datorns_slag}")
+if ditt_slag > datorns_slag:
+    print("Du vann!")
+else:
+    print("datorn vann den här gången")
